998c60cd35c65b91f1795ec06a2a0898a0ad8c24
namrur <35817877+namrur@users.noreply.github.com>
2023-05-19 10:10:06 +0300

    Update message.php

--- message.php
+++ message.php
@@ -1,13 +1,3 @@
-
-{
-“app”: “Name of the incoming message app”,
-“sender”: “A person’s name or number who is sending you the message”,
-“message”: “Incoming message”
-}
-
-{
-“reply”: “Hi, we have received your request, thanks for contacting”
-}
 <?php
 // required headers
 header("Access-Control-Allow-Origin: *");
